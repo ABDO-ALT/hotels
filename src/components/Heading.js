b09a58c96491a9440701d0af0ce1d3ae75ce74b1
ABDO-ALT <abdomohamad009639@gmail.com>
2020-02-09 15:39:07 +0100

    done

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,12 +4,12 @@ import Clock from "./Clock";
 const Heading = () => {
   return (
     <header className="App-header">
+      <span className="App-title">CYF Hotel</span>
       <img
         className="App-logo"
         src="https://image.flaticon.com/icons/svg/139/139899.svg"
         alt="logo"
       />
-      <span className="App-title">CYF Hotel</span>
       <Clock />
     </header>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,30 +1,39 @@
 import React from "react";
 import SearchButton from "./SearchButton";
 
-const Search = () => {
-  return (
-    <div className="search">
-      <div className="page-header">
-        <h4 className="text-left">Search Bookings</h4>
-      </div>
-      <div className="row search-wrapper">
-        <div className="col">
-          <form className="form-group search-box">
-            <label htmlFor="customerName">Customer name</label>
-            <div className="search-row">
-              <input
-                type="text"
-                id="customerName"
-                className="form-control"
-                placeholder="Customer Id"
-              />
-              <SearchButton />
-            </div>
-          </form>
+class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: "",
+      value: ""
+    };
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <div className="page-header">
+          <h4 className="text-left">Search Bookings</h4>
+        </div>
+        <div className="row search-wrapper">
+          <div className="col">
+            <form className="form-group search-box">
+              <label htmlFor="customerName">Customer name</label>
+              <div className="search-row">
+                <input
+                  type="text"
+                  id="customerName"
+                  className="form-control"
+                  placeholder="Customer Id"
+                />
+                <SearchButton />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 export default Search;
